9091858c603e18dc518a6485712b729650f50a9d
bhootrasourabh <bhootrasourabh@gmail.com>
2020-06-21 18:19:35 +0530

    add the sign and button component

--- src/App.js
+++ src/App.js
@@ -2,8 +2,9 @@ import React from "react";
 import HomePage from "./pages/homepage/homepage.component";
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
-import ShopPage from "./pages/shop/shop.component.jsx";
-import Header from "./components/header/header.component.jsx";
+import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
+        <Route path="/signin" component={SignInAndSignUpPage} />
       </Switch>
     </div>
   );
