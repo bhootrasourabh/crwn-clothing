6ab0ba03f79e8a8b9a126d8a3db82043569797e7
bhootrasourabh <bhootrasourabh@gmail.com>
2020-06-24 17:53:08 +0530

    finishing sign in handelSubmit method

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import FormInput from "../form-input/form-input.component";
 import CustomButton from "../custom-button/custom-button.component";
 import "./sign-in.styles.scss";
-import { signInWithGoogle } from "../../firebase/firebase.utils.js";
+import { auth, signInWithGoogle } from "../../firebase/firebase.utils.js";
 
 class SignIn extends React.Component {
   constructor() {
@@ -14,10 +14,17 @@ class SignIn extends React.Component {
     };
   }
 
-  handelSubmit = (event) => {
+  handelSubmit = async (event) => {
     event.preventDefault();
 
-    this.setState({ email: " ", password: " " });
+    const { email, password } = this.state;
+
+    try {
+      auth.signInWithEmailAndPassword(email, password);
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   handelChange = (event) => {
